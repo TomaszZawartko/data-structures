a3dfb47ab7d82f56da61d1879549ae66098a5d01
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2021-01-18 22:54:48 +0100

    squash d6 i d7

--- src/main/java/com/data/Main.java
+++ src/main/java/com/data/Main.java
@@ -40,5 +40,8 @@ public class Main {
 
         System.out.println("dupa4");
         System.out.println("dupa5");
+
+        System.out.println("dupa6");
+        System.out.println("dupa7");
     }
 }
